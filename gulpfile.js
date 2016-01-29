824f3a2ad07943d677817b29535ea06b103e6e00
diegopq <diego.pq.k@gmail.com>
2016-01-29 15:24:23 +0100

    New landing page!

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp    = require('gulp'),
+    sass    = require('gulp-sass'),
+    nano    = require('gulp-cssnano'),
+    rename  = require('gulp-rename');
+
+gulp.task('compile', function() {
+  return gulp.src('./css/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(nano({discardComments: {removeAllButFirst: true}}))
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./css/main.scss', [ 'compile' ]);
+});
